cc0c7161633cdb83063d89fe1e01588ef02ed79b
chenwuxing <chenwuxing87@163.com>
2020-09-08 15:13:54 +0800

    生产者消费者问题的条件变量实现

--- chense-up/threadSynchronization/producer_consumer.c
+++ chense-up/threadSynchronization/producer_consumer.c
@@ -3,75 +3,63 @@
 #include<pthread.h>
 #include<unistd.h>
 
-typedef struct MSG
+int bufferCount = 0;
+// 对共享缓冲区的互斥访问
+pthread_mutex_t mutex;
+// 阻塞线程
+pthread_cond_t cond;
+
+void* producer(void *arg)
 {
-    struct MSG *next;
-    struct MSG *pre;
-    int id;
-
-}msg_t;
-
-msg_t *phead = NULL, *ptail = NULL;
-
-// 静态初始化，也可以用pthread_cond_init(&hasproduct,NULLL);进行初始化
-pthread_cond_t hasProduct = PTHREAD_COND_INITIALIZER;
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-
-// 生产者
-void* producter(void *p)
-{
-    msg_t *mg = NULL;
     while(1)
     {
-        pthread_mutex_lock(&lock);
-        //生产产品
-        mg = (msg_t*)malloc(sizeof(msg_t));
-        scanf("%d",&mg->id);
-        printf("product id = %d\n",mg->id);
-        //加入到生产队列里
-        mg->next = NULL;
-        mg->pre = ptail;
-        ptail->next = mg;
-        ptail = mg;
-        pthread_mutex_unlock(&lock);
-        //唤醒至少一个阻塞在条件变量上的线程
-        pthread_cond_signal(&hasProduct);
+        pthread_mutex_lock(&mutex);
+        printf("producer put a product to buffer.");
+        bufferCount++;
+        printf("the bufferCount is %d\n",bufferCount);
+        pthread_mutex_unlock(&mutex);
+        pthread_cond_signal(&cond);
+      
     }
-    return NULL;
+    
 }
 
-//消费者
-void* consumer(void *p)
+void* consumer(void *arg)
 {
-    msg_t *mg = NULL;
     while(1)
     {
-        pthread_mutex_lock(&lock);
-        while(phead == ptail)
+        pthread_mutex_lock(&mutex);
+        while(bufferCount == 0)
         {
-            //注意这里不能是if，因为会有多个消费者争夺资源
-            //当队列没有产品的时候会阻塞且释放锁直到被唤醒
-            // 有产品被唤醒的时候会返回且重新获得锁
-            pthread_cond_wait(&hasProduct,&lock);
+            //函数会对互斥锁进行解锁
+            pthread_cond_wait(&cond,&mutex);
+            //解除阻塞后，会对互斥锁进行加锁
         }
-        printf("consume id = %d\n",ptail->id);
-        //消费一个产品
-        ptail = ptail->pre;
-        free(ptail->next);
-        ptail->next = NULL;
-        pthread_mutex_unlock(&lock);
+        printf("consumer get a product from buffer.");
+        bufferCount--;
+        printf("the bufferCount is %d\n",bufferCount);
+        pthread_mutex_unlock(&mutex);
+
     }
 }
 
 int main()
 {
-    phead = (msg_t*)malloc(sizeof(msg_t));
-    ptail = phead;
-    pthread_t pid,cid;
-    pthread_create(&pid,NULL,producter,NULL);
-    pthread_create(&cid,NULL,consumer,NULL);
-
-    pthread_join(pid,NULL);
-    pthread_join(cid,NULL);
-    return 0;
+    pthread_t thrdProc,thrdCons;
+    if(pthread_create(&thrdProc,NULL,producer,NULL) != 0)
+        printf("create thread failed.");
+    if(pthread_create(&thrdCons,NULL,consumer,NULL) != 0)
+        printf("create thread failed.");
+    
+    //等待线程结束
+    if(pthread_join(thrdProc,NULL) != 0)
+        printf("wait thread failed.");
+    if(pthread_join(thrdCons,NULL) != 0)
+        printf("wait thread failed.");
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&cond);
+    
 }
+
+
+
